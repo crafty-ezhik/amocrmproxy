3aaa18fea13bf8d077650a6137c49214e8c205ac
Alexandr <alexandr@biznes-len.com>
2025-06-26 15:12:53 +0300

    feat: add make request function and custom type

--- utils/custom_types.go
+++ utils/custom_types.go
@@ -1,1 +1,15 @@
 package utils
+
+const (
+	HeaderContentType   = "Content-Type"
+	HeaderAuthorization = "Authorization"
+)
+
+type AnyMap map[string]interface{}
+
+// Response - Структура для отдачи ответа клиенту
+type Response struct {
+	Body   []byte
+	Status int
+	Error  error
+}
--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,66 @@
 package utils
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+)
+
+// MakeRequest - Делает запрос по указанному url. Добавляет заголовок Authorization, значение которого
+// берется из пришедшего запроса. Добавление body происходит в зависимости от его наличии в параметрах.
+//
+// Возвращает указатель на структуру Response
+func MakeRequest(r *http.Request, c *http.Client, method string, url string, body []byte) *Response {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return &Response{
+			Body:   nil,
+			Status: http.StatusInternalServerError,
+			Error:  err,
+		}
+	}
+
+	if body != nil {
+		req.Body = io.NopCloser(bytes.NewReader(body))
+	}
+
+	req.Header.Set(HeaderContentType, "application/json")
+	if token := r.Header.Get(HeaderAuthorization); token != "" {
+		req.Header.Set(HeaderAuthorization, r.Header.Get(HeaderAuthorization))
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return &Response{
+			Body:   nil,
+			Status: http.StatusInternalServerError,
+			Error:  err,
+		}
+	}
+	defer resp.Body.Close()
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return &Response{
+			Body:   nil,
+			Status: http.StatusInternalServerError,
+			Error:  err,
+		}
+	}
+	return &Response{
+		Body:   respBody,
+		Status: resp.StatusCode,
+		Error:  nil,
+	}
+}
+
+func SendResponse(w http.ResponseWriter, response *Response) {
+	w.WriteHeader(response.Status)
+
+	if response.Error != nil {
+		w.Write([]byte(response.Error.Error()))
+		return
+	}
+
+	w.Header().Set(HeaderContentType, "application/json; charset=UTF-8")
+	w.Write(response.Body)
+}
